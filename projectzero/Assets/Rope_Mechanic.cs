04e1c97715090fd9dcb5a8c6b853a9421037e06d
yunusemreakkas16 <Y.Emre_Akkas@outlook.com>
2022-11-03 00:22:21 +0300

    Rope_Mechanic
    Rope mechanics are still in progress.

---
+++ projectzero/Assets/Rope_Mechanic.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope_Mechanic : MonoBehaviour
+{
+    public GameObject player;
+    
+    void Start()
+    {
+        player = GameObject.Find("Rogue_05");
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
