adf16c51c290c1eec13b39aa3e7b9a47d8429631
NNouveau <hyakupehlivan@gmail.com>
2022-11-09 17:13:41 +0300

    Gerekli düzenlemeler yapıldı

--- projectzero/Assets/Scripts/Movements.cs
+++ projectzero/Assets/Scripts/Movements.cs
@@ -1,18 +1,23 @@
+
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+
 
 public class Movements : MonoBehaviour
 {
     [Header("Components")]
     public Rigidbody2D rb;
     public Animator animator;
+
     public LayerMask groundLayer;
 
 
+
     [Header("Movements")]
-    public float runSpeed = 8f, jumpStr = 10f, fallMultiplier = 5f, gravity = 1f, jumpDelay = 0.25f;
-    private float jumpTimer;
+    public float runSpeed = 8f, jumpStr = 10f, fallMultiplier = 5f, gravity = 1f, jumpDelay = 0.25f, ACC = 9f, DCC = 9f, velPower = 1.2f;
+    //private float jumpTimer;
     private bool face = true;
     public Vector3 velocity;
     public Vector2 direction;
@@ -33,13 +38,12 @@ public class Movements : MonoBehaviour
 
     void Update()
     {
-        velocity.x = rb.velocity.x;
-        velocity.y = rb.velocity.y;
+
         direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         onGround = Physics2D.Raycast(transform.position + colliderOffset, Vector2.down, groundLenght, groundLayer) || Physics2D.Raycast(transform.position - colliderOffset, Vector2.down, groundLenght, groundLayer);
         if (Input.GetKey(KeyCode.W))
         {
-            jumpTimer = Time.time + jumpDelay;
+            //jumpTimer = Time.time + jumpDelay;
         }
 
     }
@@ -48,13 +52,17 @@ public class Movements : MonoBehaviour
     //For some optimization
     private void FixedUpdate()
     {
+        velocity.x = rb.velocity.x;
+        velocity.y = rb.velocity.y;
+
+
         modifyPhysics();
         if (!gameObject.GetComponent<Abilities>().isAttached)
         {
             run(direction.x);
         }
-        
-        if (jumpTimer > Time.time&& onGround)
+
+        if (Input.GetKey(KeyCode.W) && onGround) //(jumpTimer > Time.time && onGround)
         {
             jump();
         }
@@ -73,8 +81,16 @@ public class Movements : MonoBehaviour
     //Running function
     public void run(float horizontal)
     {
-        velocity = new Vector3(Input.GetAxis("Horizontal"), 0f);
-        transform.position += velocity * runSpeed * Time.deltaTime;
+        float targetSpeed = horizontal * runSpeed;
+        float speedDif = targetSpeed - rb.velocity.x;
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.00f) ? ACC : DCC;
+        float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
+        rb.AddForce(movement * Vector2.right);
+
+
+
+
+
         animator.SetFloat("HorizontalSpeed", Mathf.Abs(velocity.x));
         if ((horizontal > 0 && !face) || (horizontal < 0 && face))
         {
@@ -86,9 +102,11 @@ public class Movements : MonoBehaviour
     //Jumping function
     public void jump()
     {
+
+
         rb.velocity = new Vector2(rb.velocity.x, 0);
         rb.AddForce(Vector2.up * jumpStr, ForceMode2D.Impulse);
-        jumpTimer = 0;
+        //jumpTimer = 0;
     }
 
 
@@ -102,7 +120,7 @@ public class Movements : MonoBehaviour
         else
         {
             rb.gravityScale = gravity;
-            //rb.drag = linearDrag * 0.15f;
+            rb.drag = linearDrag * 0.15f;
             if (rb.velocity.y < 0)
             {
                 rb.gravityScale = gravity * fallMultiplier;
